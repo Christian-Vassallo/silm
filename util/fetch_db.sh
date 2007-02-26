62802a4a47191a9ac5650a1059c3e9b7289d4c0e
elven <elven>
2007-02-26 19:03:38 +0000

    r58: fix password parameter

--- util/fetch_db.sh
+++ util/fetch_db.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
 RPASS=$1
-LPASS=$0
 
 set -e
 
 if [ "$RPASS"x = "x" ]; then
-	echo "Syntax: $0 localpass remotepass"
+	echo "Syntax: $0 remotepass"
 	exit 1
 fi
 
-echo "drop database nwserver" | mysql -uroot -p${LPASS}
-
-ssh root@t mysqldump -uroot -p${RPASS} \
-	--add-drop-database \
-	-c -e -l  silm_nwserver | mysql -uroot -p${LPASS}
+ssh root@t mysqldump -usilm_nwserver -p${RPASS} \
+	--add-drop-table \
+	-c -e -l  silm_nwserver | mysql -uroot nwserver
