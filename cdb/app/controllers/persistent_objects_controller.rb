187a635fab7019414cbc36f03377329403f4ee28
elven <elven>
2007-02-27 20:12:47 +0000

    r75: id 77: minor unspecified bugfixes

--- cdb/app/controllers/persistent_objects_controller.rb
+++ cdb/app/controllers/persistent_objects_controller.rb
@@ -1,73 +1,66 @@
 class PersistentObjectsController < ApplicationController
-	before_filter :authenticate_object_admin
-	layout "main"
+  before_filter :authenticate_object_admin
+  layout "main"
 
-	def index
-		list
-		render :action => 'list'
-	end
+  def index
+    list
+    render :action => 'list'
+  end
 
-	# GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
-	verify :method => :post, :only => [ :destroy, :create, :update ],
-		:redirect_to => { :action => :list }
+  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
+  verify :method => :post, :only => [ :destroy, :create, :update ],
+    :redirect_to => { :action => :list }
 
-	def list
-		cond_str = '1=1'
-		cond = []
-		@filter_moveables = params['filter_moveables'] || session['po_filter_moveables'] || false
-		session['po_filter_moveables'] = params['filter_moveables'] if params['filter_moveables']
-				
-		@filter_str = params['filter_str'] || session['po_filter'] || 'blut bloodstain knochen'
-		session['po_filter'] = params['filter_str'] if params['filter_str']	
+  def list
+    cond_str = '1=1'
+    cond = []
+        
+    @filter_str = params['filter_str'] || session['po_filter'] || 'blut bloodstain knochen move_'
+    session['po_filter'] = params['filter_str'] if params['filter_str'] 
 
-		if @filter_moveables
-			cond_str += ' and resref not like ?'
-			cond << 'move_%'
-		end
+    @filter_str.split(/\s+/).map {|x| x.gsub(/[^a-z]/i, "")}.each {|x|
+      cond_str += ' and resref not like ? '
+      cond << '%' + x + '%'
+    }
+    @persistent_object_pages, @persistent_objects = paginate :persistent_objects, 
+      :conditions => [cond_str].concat(cond), 
+      :per_page => 100
+  end
 
-		@filter_str.split(/\s+/).map {|x| x.gsub(/[^a-z]/i, "")}.each {|x|
-			cond_str += ' and resref not like ? '
-			cond << '%' + x + '%'
-		}
-		@persistent_object_pages, @persistent_objects = paginate :persistent_objects, 
-			:conditions => [cond_str].concat(cond), 
-			:per_page => 100
-	end
+  def show
+    @persistent_object = PersistentObject.find(params[:id])
+  end
 
-	def show
-		@persistent_object = PersistentObject.find(params[:id])
-	end
+  def new
+    @persistent_object = PersistentObject.new
+  end
 
-	def new
-		@persistent_object = PersistentObject.new
-	end
+  def create
+    @persistent_object = PersistentObject.new(params[:persistent_object])
+    if @persistent_object.save
+      flash[:notice] = 'PersistentObject was successfully created.'
+      redirect_to :action => 'list'
+    else
+      render :action => 'new'
+    end
+  end
 
-	def create
-		@persistent_object = PersistentObject.new(params[:persistent_object])
-		if @persistent_object.save
-			flash[:notice] = 'PersistentObject was successfully created.'
-			redirect_to :action => 'list'
-		else
-			render :action => 'new'
-		end
-	end
+  def edit
+    @persistent_object = PersistentObject.find(params[:id])
+  end
 
-	def edit
-		@persistent_object = PersistentObject.find(params[:id])
-	end
+  def update
+    @persistent_object = PersistentObject.find(params[:id])
+    if @persistent_object.update_attributes(params[:persistent_object])
+      flash[:notice] = 'PersistentObject was successfully updated.'
+      redirect_to :action => 'show', :id => @persistent_object
+    else
+      render :action => 'edit'
+    end
+  end
 
-	def update
-		@persistent_object = PersistentObject.find(params[:id])
-		if @persistent_object.update_attributes(params[:persistent_object])
-			flash[:notice] = 'PersistentObject was successfully updated.'
-			redirect_to :action => 'show', :id => @persistent_object
-		else
-			render :action => 'edit'
-		end
-	end
-
-	def destroy
-		PersistentObject.find(params[:id]).destroy
-		redirect_to :action => 'list'
-	end
+  def destroy
+    PersistentObject.find(params[:id]).destroy
+    redirect_to :action => 'list'
+  end
 end
