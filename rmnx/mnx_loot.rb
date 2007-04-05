21c1351af8244305515b26cf99dda2b79e0ecdc6
elven <elven>
2007-04-05 21:28:01 +0000

    r168: chance

--- rmnx/mnx_loot.rb
+++ rmnx/mnx_loot.rb
@@ -18,19 +18,19 @@ class LootAggregator < RMNX::CommandSpace
 		ActiveRecord::Base.connection.instance_eval {@connection.reconnect = true}
 	end
 	
-	def mnx_getloot racial_type, resref, tag, name, lvar
+	def mnx_getloot racial_type, resref, tag, name, lvar = ""
 		s = ''
 		a = []
 
-		loot = Loot::find_by_sql(['select loot from %s where ' +
-			'(racial_type = 0 or racial_type = ?) and ' +
-				'(? like resref) and ' +
-				'(? like tag) and ' +
-				'(? like name) and ' +
-				'(? like lvar) ' +
-			'order by racial_type, resref, tag, lvar asc',
-				'loots', 
-				racial_type, resref, tag, lvar
+		loot = Loot::find_by_sql(['select loot from loots where ' +
+				'rand() - chance <= 0 and ' +
+				'(racial_type = -1 or racial_type = ?) and ' +
+				'(resref = \'\' or ? like resref) and ' +
+				'(tag = \'\' or ? like tag) and ' +
+				'(name = \'\' or ? like name) and ' +
+				'(lvar = \'\' or ? like lvar) ' +
+				'order by racial_type, resref, tag, lvar asc',
+			racial_type, resref, tag, name, lvar
 		])
 		loot = loot.map{|x| x.loot}.join("#").gsub(/##+/, '#')
 		return loot
