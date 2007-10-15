829d0fca7b2042cc199b4ee2d4c10936bdfb4325
Bernhard Stoeckner <elven@swordcoast.net>
2007-10-15 18:37:10 +0200

    [doc] add schemata for gv

---
+++ schema/settings.sql
@@ -0,0 +1,12 @@
+CREATE TABLE gv (
+    id serial unique primary key not null,
+    "key" character varying NOT NULL,
+    "type" character varying DEFAULT 'int'::character varying,
+    
+	value text NOT NULL,
+    amask integer DEFAULT 0 not null,
+
+    CONSTRAINT gv_type_check CHECK ((("type")::text = ANY ((ARRAY['int'::character varying, 'string'::character varying, 'float'::character varying])::text[]))),
+
+	unique ("key", "type")
+);
