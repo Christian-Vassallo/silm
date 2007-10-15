656907998d6c4e8794893e13dda0fec0f4337b47
Bernhard Stoeckner <elven@swordcoast.net>
2007-10-15 18:24:53 +0200

    [doc] add schemata for functions

---
+++ schema/functions.sql
@@ -0,0 +1,26 @@
+CREATE FUNCTION clamp(thisc integer, min integer, max integer) RETURNS integer
+    AS $_$select case when $1 < $2 then $2 else (case when $1 > $3 then $3 else $1 end) end$_$
+    LANGUAGE sql;
+
+
+CREATE FUNCTION lowest_int(integer, integer) RETURNS integer
+    AS $_$select case when ($1 > $2) then $2 else $1 end$_$
+    LANGUAGE sql IMMUTABLE STRICT;
+
+
+CREATE FUNCTION sha1(text) RETURNS text
+    AS $_$
+SELECT
+ENCODE(public.DIGEST($1, 'sha1'),'hex') AS result
+$_$
+    LANGUAGE sql;
+
+
+CREATE FUNCTION unix_timestamp() RETURNS integer
+    AS $$select date_part('epoch', now())::int;$$
+    LANGUAGE sql STABLE;
+
+
+CREATE FUNCTION unixts() RETURNS integer
+    AS $$select date_part('epoch', now())::int;$$
+    LANGUAGE sql STABLE;
