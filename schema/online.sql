baa28f98d6baaea9f1459021788de662ccf7932d
Bernhard Stoeckner <elven@swordcoast.net>
2008-01-08 16:38:46 +0100

    [schema] add online_aggregator view

--- schema/online.sql
+++ schema/online.sql
@@ -13,3 +13,13 @@ CREATE TABLE online (
     afk boolean DEFAULT false NOT NULL,
     area_s character varying DEFAULT ''::character varying
 );
+
+create view online_aggregator as
+	SELECT
+		online.id, online.aid, online.cid, online.account, online."character", 
+		online.dm, online.area, online.x, online.y, online.z, online.f, online.afk, online.area_s,
+		characters.race, characters.subrace, characters.xp, characters.class1, 
+		characters.class1_level, characters.class2, characters.class2_level, 
+		characters.class3, characters.class3_level
+   FROM online
+		LEFT JOIN characters ON characters.id = online.cid;
