1f5b7a1afbc5eed5d12d1753af94e89c1830118a
Bernhard Stoeckner <elven@swordcoast.net>
2008-02-03 14:15:31 +0100

    [schema/scorco] add support for dropped_items, add maint. view

--- schema/scorco.sql
+++ schema/scorco.sql
@@ -28,6 +28,16 @@ create table scorco.object_metadata (
 
 ) inherits (scorco.object_ids);
 
+create view scorco.metadata_info as 
+	select
+		p.relname,id,create_on,last_access_on,resref,tag,name,area_resref,area_tag,x,y,z,f
+		from scorco.object_metadata c, pg_class p
+		where c.tableoid = p.oid;
+
 create function scorco.touch_object(int) returns int
 	as $$ update scorco.object_ids set last_access_on = now() where id = $1; select $1 $$
 	language sql;
+
+
+create table scorco.dropped_items (
+) inherits (scorco.object_metadata);
