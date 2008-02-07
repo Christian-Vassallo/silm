4578e2bf6c8ad7350e3dfb9a0eb0b06396f0ae08
Bernhard Stoeckner <elven@swordcoast.net>
2008-02-07 21:56:05 +0100

    Schema/nss: add support for proper Location/Vector/Area serialisation

--- schema/base.sql
+++ schema/base.sql
@@ -8,5 +8,9 @@ create domain resref as varchar
 create domain tag as varchar
 	check (length(VALUE) > 0 and length(VALUE) <= 32);
 
+create type vector as (x float, y float, z float);
+create type area as (tag tag, resref resref);
+create type location as (area area, position vector, facing float);
+
 -- create domain objecttype as varchar
 --	check (VALUE = any(ARRAY['']));
