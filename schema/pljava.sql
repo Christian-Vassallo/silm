2c69f02b6b8158857d4e3f32a245d0410b1c0514
Bernhard Stoeckner <elven@swordcoast.net>
2008-03-02 15:19:36 +0100

    update pljava hooks/nwn-tools to do proper charset conversion

--- schema/pljava.sql
+++ schema/pljava.sql
@@ -1,6 +1,5 @@
 select sqlj.install_jar('file:/home/silm/sqlgffxml.jar', 'sqlgffxml', true);
-select sqlj.install_jar('file:/home/silm/nwn-tools.jar', 'nwntools', true);
 select sqlj.install_jar('file:/home/silm/log4j.jar', 'log4j', true);
-select sqlj.install_jar('file:/home/silm/meta-jb-util.jar', 'm1', true);
-select sqlj.install_jar('file:/home/silm/meta-jb-xml.jar', 'm2', true);
-select sqlj.set_classpath('public', 'sqlgffxml:nwntools:log4j:m1:m2');
+select sqlj.install_jar('file:/home/silm/meta-jb-util.jar', 'metajbutil', true);
+select sqlj.install_jar('file:/home/silm/meta-jb-xml.jar', 'metajbxml', true);
+select sqlj.set_classpath('public', 'sqlgffxml:log4j:metajbutil:metajbxml');
