8d7819bbab9da63a09f7b4620fb5e2e7233d30f2
Bernhard Stoeckner <elven@swordcoast.net>
2008-03-03 08:34:35 +0100

    remove plgff overhead from pljava

--- schema/gff.sql
+++ schema/gff.sql
@@ -64,7 +64,7 @@ $_$ language sql;
 create function gff.gfftotext(varchar,bytea) returns text as 'net.swordcoast.sternenfall.ConvertShit.gffToXml(java.lang.String,byte[])' language javau;
 create function gff.texttogff(varchar,boolean) returns bytea as 'net.swordcoast.sternenfall.ConvertShit.XmlToGff(java.lang.String,boolean)' language javau;
 create function gff.toxml(name varchar,gff gff) returns xml as 'select gff.gfftotext($1, $2::bytea)::xml;' language sql;
-create function gff.toxml(gff gff) returns xml as $_$select gff.gfftotext(gff.value($1, '/TemplateResRef' ), $1::bytea)::xml;$_$ language sql;
+create function gff.toxml(gff gff) returns xml as $_$select gff.gfftotext('_runtime', $1::bytea)::xml;$_$ language sql;
 create function gff.togff(xml,compression boolean) returns gff as 'select gff.texttogff($1::varchar, $2)::gff;' language sql;
 create function gff.togff(xml) returns gff as 'select gff.texttogff($1::varchar, false)::gff;' language sql;
 
