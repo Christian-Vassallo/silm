9e4623c5544440e906a8f16efd4c66b2b1a32cb8
Bernhard Stoeckner <elven@swordcoast.net>
2008-04-22 11:52:59 +0200

    macro: rename __sp_extern -> extern

--- mod/nss/global/gpp.h
+++ mod/nss/global/gpp.h
@@ -1,8 +1,8 @@
 #ifndef GPP_H
 #define GPP_H
 
-// Macro: __sp_extern(x)
+// Macro: extern(x)
 // a passthru-token for externalised file includes by the nwscript compiler.
-#define __sp_extern(x) #include(x)
+#define extern(x) #include(x)
 
 #endif
