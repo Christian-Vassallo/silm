1f4785ef8331da2d8f89f3d44b10e6b6c0dd8caa
Bernhard Stoeckner <elven@swordcoast.net>
2008-05-26 18:21:04 +0200

    add the old audit facilities from Silbermarken

--- mod/nss/global/core.h
+++ mod/nss/global/core.h
@@ -2,12 +2,7 @@
 #define CORE_H
 
 // These are included automagically for each file because they are core files
-#ifndef PGSQL_N
 #include "inc_pgsql.nh"
-#endif
-
-#ifndef EVENTS_N
 #include "inc_events.nh"
-#endif
 
 #endif
