15a2dcfa07456cdfa54441c0b6448083c3d42087
Bernhard Stoeckner <elven@swordcoast.net>
2009-01-04 19:12:08 +0100

    [HACK] rmnx weather libs loaded through pgsql
    Ugly, but the best I can do right now.

--- schema/weather.sql
+++ schema/weather.sql
@@ -12,3 +12,14 @@ CREATE TABLE weather_overrides (
     wind integer NOT NULL,
     prec integer NOT NULL
 );
+
+create schema weather;
+
+create function weather.getweather(resref varchar, tileset varchar, mask int, year int, month int, day int, hour int, minute int) returns bytea as
+$_$
+	require 'rmnx';
+	require 'mnx_temp';
+	r = CommandTemperature.new
+	return r.mnx_getweather(resref, tileset, mask, year, month, day, hour, minute)
+$_$ language 'plruby';
+
