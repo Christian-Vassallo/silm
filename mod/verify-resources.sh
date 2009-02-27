e7b1454ca7f1d2175babb3acdde2c987f58ff86b
Bernhard Stoeckner <elven@swordcoast.net>
2009-02-27 14:58:58 +0100

    verify-resources: only verify actual resource directories

--- mod/verify-resources.sh
+++ mod/verify-resources.sh
@@ -5,7 +5,7 @@
 
 modroot=$(readlink -f `dirname $0`)
 
-too_long_files=$(find $modroot -maxdepth 2 | cut -d'.' -f2 | cut -d'/' -f3- | sort -u | egrep '/.{17,}$')
+too_long_files=$(find $modroot/{ut*,area,dlg,itp,mod,nss,ssf}/ -maxdepth 1 | cut -d'.' -f2 | cut -d'/' -f3- | sort -u | egrep '/.{17,}$')
 
 [ ! -z "$too_long_files" ] && echo -e "Files that are too long:\n$too_long_files" >&2 && exit 1
 
