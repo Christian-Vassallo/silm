54850e4e4a405e8735f38b3167fe019fde501105
Bernhard Stoeckner <elven@swordcoast.net>
2010-01-28 21:15:57 +0100

    filter/fix_facings: strip -0.0 too

--- mod/filters/fix_facings.rb
+++ mod/filters/fix_facings.rb
@@ -13,7 +13,7 @@ self.each_by_flat_path do |label, field|
 	next unless field.is_a?(Gff::Field)
 	next unless field.field_type == :float
 	next unless label =~ %r{\[\d+\]/(Facing|Bearing)$}
-	if field.field_value < 0
+	if field.field_value <= 0
 		field.field_value = field.field_value.abs
 		count += 1
 	end
