32021db6d7cf400d1913a35c5e33e6b796585cd6
Bernhard Stoeckner <elven@swordcoast.net>
2010-05-27 20:34:13 +0200

    inc_telepathbond: support not sending heard, spoken; some format changes

--- schema/telepathic_bonds.sql
+++ schema/telepathic_bonds.sql
@@ -2,10 +2,12 @@ CREATE TABLE telepathic_bonds (
     id serial primary key,
     "character" integer references characters not null,
     t_character integer references characters not null,
-    ts timestamp with time zone DEFAULT now(),
-    active boolean DEFAULT true,
-    shortname character varying,
-    expire integer DEFAULT 0 NOT NULL,
+    ts timestamp not null default now(),
+    active boolean not null default true,
+	send_heard boolean not null default false,
+	send_spoken boolean not null default false,
+    shortname not null character varying,
+    expire integer not null defaul t0,
 	
 	unique ("character", t_character),
 	
