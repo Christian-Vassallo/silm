c9aedbb431f04666ba7f125668fe1cf8ea375349
Bernhard Stoeckner <elven@swordcoast.net>
2011-01-11 16:31:19 +0100

    filters/fix_static: pvq trans/shiny fix rev 2

--- mod/filters/fix_static.rb
+++ mod/filters/fix_static.rb
@@ -3,13 +3,22 @@
 want :git
 
 count = 0
+pvq = 0
 
 (self / 'Placeable List$').each {|item|
-  if (item / 'Useable$') == 0 && (item / 'Static$') == 0 &&
-      (item / 'TemplateResRef$') !~ /^pvq_/
-    (item / 'Static').v = 1
-    count += 1
+  case (item / 'TemplateResRef$')
+    # hotfix: project q placeables use shinytextures that
+    # don't do proper transparency when static. force non-static.
+    when /^pvq_/
+      (item / 'Static').v = 0
+      pvq += 1
+    else
+      if (item / 'Useable$') == 0 && (item / 'Static$') == 0
+        (item / 'Static').v = 1
+        count += 1
+      end
   end
 }
 
 log "#{count} placeables made static."
+log "#{pvq} placeables forced non-static."
