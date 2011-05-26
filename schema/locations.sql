f22535ecb93fc73d90cf25b2e517ecdc93e6f8e0
Bernhard Stoeckner <elven@swordcoast.net>
2011-05-26 12:36:41 +0200

    /go: support for player-accessible go points

--- schema/locations.sql
+++ schema/locations.sql
@@ -4,5 +4,6 @@ CREATE TABLE locations (
     area character varying NOT NULL,
     x double precision DEFAULT 0 NOT NULL,
     y double precision DEFAULT 0 NOT NULL,
-    z double precision DEFAULT 0 NOT NULL
+    z double precision DEFAULT 0 NOT NULL,
+	restricted boolean default true not null
 );
